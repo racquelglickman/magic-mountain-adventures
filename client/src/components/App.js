522cd2c26714762feb2e06bdb1badecaeeebdaa1
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-31 17:01:08 -0400

    creates login authentification baby

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route, Routes } from "react-router-dom";
-import MyProvider from "./MyProvider";
+import React, { useContext, useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import MyProvider, { MyContext } from "./MyProvider";
 import Home from "./Home";
 import Login from "./Login";
 import AttractionDetails from "./AttractionDetails";
@@ -8,21 +8,34 @@ import UserProfile from "./UserProfile";
 import Error from "./Error";
 
 function App() {
-  
-return (
-  <div>
-    <MyProvider>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/attractions/:id' element={<AttractionDetails />} />
-      <Route path='/profile' element={<UserProfile />} />
-      <Route path='*' element={<Error />} />
-    </Routes>
-  </MyProvider>
-  </div>
-  
-)
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    // auto-login
+    fetch("/check_session").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin= {setUser}/>;
+    
+  return (
+    <div>
+      <MyProvider>
+      <Routes>
+        <Route path="/" element={<Home setUser= {setUser}/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/attractions/:id' element={<AttractionDetails />} />
+        <Route path='/profile' element={<UserProfile />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </MyProvider>
+    </div>
+    
+  )
   
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-function Home() {
+function Home({ setUser }) {
+
+  function handleLogout() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+
   return (
     <div>
         <h1>Hello Theme Park Lovers</h1>
+        <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
