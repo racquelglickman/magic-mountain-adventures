797edf14a3b765085d34ca61ab6de4986dfb35a1
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-31 11:42:42 -0400

    updates json dependencies to be most recent versions

--- client/src/components/MyProvider.js
+++ client/src/components/MyProvider.js
@@ -4,15 +4,13 @@ export const MyContext = createContext()
 
 function MyProvider({ children }) {
 
-    pass
+    return (
+        <MyContext.Provider
+            // value={({pass: pass})}
+        >
+            {children}
+        </MyContext.Provider>
+    )
 }
-
-return (
-    <MyContext.Provider
-        value={pass}
-    >
-        {children}
-    </MyContext.Provider>
-)
 
 export default MyProvider
