bc28c9cf53117590efcd380a09ddb3def3981734
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-31 11:04:36 -0400

    builds skeleton of routes and context

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Routes } from "react-router-dom";
+import MyProvider from "./MyProvider";
+import Home from "./Home";
+import Login from "./Login";
+import AttractionDetails from "./AttractionDetails";
+import UserProfile from "./UserProfile";
+import Error from "./Error";
 
 function App() {
-  // Code goes here!
+  
+return (
+  <div>
+    <MyProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/attractions/:id' element={<AttractionDetails />} />
+      <Route path='/profile' element={<UserProfile />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
+  </MyProvider>
+  </div>
+  
+)
+  
 }
 
 export default App;
---
+++ client/src/components/MyProvider.js
@@ -0,0 +1,18 @@
+import { useState, useEffect, createContext } from "react"
+
+export const MyContext = createContext()
+
+function MyProvider({ children }) {
+
+    pass
+}
+
+return (
+    <MyContext.Provider
+        value={pass}
+    >
+        {children}
+    </MyContext.Provider>
+)
+
+export default MyProvider
