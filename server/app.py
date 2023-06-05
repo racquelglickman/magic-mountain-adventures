b8470af49cdf823731e5651abd211a875b3720e7
Racquel Glickman <racquel.glickman@gmail.com>
2023-06-05 11:39:54 -0400

    started adding some routes

--- server/app.py
+++ server/app.py
@@ -3,14 +3,65 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import User, Attraction, Adventure
 
 # Views go here!
+@app.route('/')
+def home():
+    return 'Magic Mountain Adventures!'
+
+class Users(Resource):
+    def get(self):
+        return [user.to_dict() for user in User.query.all()], 200
+    
+    def post(self):
+        try:
+            new_user = User(
+                username=request.form['username'],
+                first_name=request.form['first_name'],
+                last_name=request.form['first_name'],
+                height=request.form['height']
+            )
+        
+            db.session.add(new_user)
+            db.session.commit()
+
+            new_user_dict = new_user.to_dict()
+
+            return new_user_dict, 201
+        except:
+            return {'error': '400: Validation error'}, 400
+    
+api.add_resource(Users, '/users')
+
+class UserById(Resource):
+    def get(self, id):
+        user = User.query.filter_by(id=id).first()
+
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {'error': '404: User not found'}, 404
+        
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+
+            response = make_response("", 204)
+
+            return response
+
+        return {'error': "User not found"}, 404
+        
+api.add_resource(UserById, '/users/<int:id>')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
