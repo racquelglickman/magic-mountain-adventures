16629afd5a89b59c3be6918a6a24d796cd32659c
Patrick Roberts <patrob22@gmail.com>
2023-06-05 11:53:48 -0400

    make_attraction function created in seed file

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,28 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Attraction, Adventure
+
+attractions = []
+
+def make_attraction(name, type, thrill, height):
+    new_attraction = Attraction(
+        name = name,
+        type = type,
+        thrill_level = thrill,
+        height_req = height,
+    )
+    
+    attractions.append(new_attraction)
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
-        # Seed code goes here!
+
+        db.session.delete(User)
+        db.session.delete(Attraction)
+        db.session.delete(Adventure)
+        
+
+
