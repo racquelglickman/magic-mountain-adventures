ee4ef0af050708e95f2d82868ef7b3f751eaa42f
Patrick Roberts <patrob22@gmail.com>
2023-06-05 10:25:57 -0400

    initial user, attraction and adventure models declared

--- server/models.py
+++ server/models.py
@@ -3,3 +3,11 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    pass
+
+class Attraction(db.Model, SerializerMixin):
+    pass
+
+class Adventure(db.Model, SerializerMixin):
+    pass
