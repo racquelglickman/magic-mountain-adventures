95a5b243067bfefff70a6c46f5a4ccdee1c632d2
Jolie Gielchinsky <jolieschae@gmail.com>
2023-06-05 15:53:24 -0400

    basic navbar layout implemented with links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route, Routes } from "react-router-dom";
 import MyProvider from "./MyProvider";
 import Home from "./Home";
+import Nav from "./Nav";
 import Login from "./Login";
 import AttractionDetails from "./AttractionDetails";
 import UserProfile from "./UserProfile";
 import Error from "./Error";
+import PlanAdventure from "./PlanAdventure";
 
 function App() {
   
 return (
   <div>
     <MyProvider>
+    <Nav/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/attractions/:id' element={<AttractionDetails />} />
+      <Route path='/adventure' element={<PlanAdventure />} />
       <Route path='/profile' element={<UserProfile />} />
       <Route path='*' element={<Error />} />
     </Routes>
---
+++ client/src/components/PlanAdventure.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PlanAdventure() {
+  return (
+    <div>PlanAdventure</div>
+  )
+}
+
+export default PlanAdventure
