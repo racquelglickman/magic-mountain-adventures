2997bb6a0878f857220c6f4ac4ef226c5440b304
Jolie Gielchinsky <jolieschae@gmail.com>
2023-06-07 09:40:05 -0400

    attraction container carousel

--- client/src/components/AttractionsContainer.js
+++ client/src/components/AttractionsContainer.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import AttractionCard from './AttractionCard'
-import './attractionCard.css'
+import React from 'react';
+import AttractionCard from './AttractionCard';
+import './attractionCard.css';
 
-function AttractionsContainer({ filteredList }) {
-  
-const attractionsToRender = filteredList.map((attraction) => {
-  return <AttractionCard key={attraction.id} attraction={attraction} />
-})
+function AttractionsContainer({ filteredList, currentPage, attractionsPerPage }) {
+  const indexOfLastAttraction = currentPage * attractionsPerPage;
+  const indexOfFirstAttraction = indexOfLastAttraction - attractionsPerPage;
+  const attractionsToRender = filteredList.slice(indexOfFirstAttraction, indexOfLastAttraction).map((attraction) => {
+    return <AttractionCard key={attraction.id} attraction={attraction} />;
+  });
 
   return (
     <div className="attractionContainer">
       {attractionsToRender}
     </div>
-  )
+  );
 }
 
-export default AttractionsContainer
+export default AttractionsContainer;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,38 +1,59 @@
-import React, { useContext, useState } from 'react'
-import Search from './Search'
-import AttactionsContainer from './AttractionsContainer'
-import { MyContext } from './MyProvider'
-import ToggleSwitch from './ToggleSwitch'
+import React, { useContext, useState } from 'react';
+import Search from './Search';
+import AttractionsContainer from './AttractionsContainer';
+import { MyContext } from './MyProvider';
+import ToggleSwitch from './ToggleSwitch';
 
 function Home() {
+  const { attractions, user } = useContext(MyContext);
 
-  const { attractions, user } = useContext(MyContext)
-  
-  //handles filter of attractions
-  const [attractionSearch, setAttractionSearch] = useState('')
+  const [attractionSearch, setAttractionSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const attractionsPerPage = 6;
 
   function handleSearch(target) {
-    setAttractionSearch(target)
+    setAttractionSearch(target);
+    setCurrentPage(1); // Reset to the first page when the search query changes
   }
-  
-  const filteredList = attractions.filter((attraction => {
-    return attraction.name.toLowerCase().includes(attractionSearch.toLowerCase())
-  }))
 
-  //toggles attractions or adventures
-  const [toggleAdventures, setToggleAdventures] = useState(false)
+  const filteredList = attractions.filter((attraction) => {
+    return attraction.name.toLowerCase().includes(attractionSearch.toLowerCase());
+  });
 
-  function handleToggle() {
-    setToggleAdventures(!toggleAdventures)
-  }
+  const handleNextPage = () => {
+    const lastPage = Math.ceil(filteredList.length / attractionsPerPage);
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, lastPage));
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const [toggleAdventures, setToggleAdventures] = useState(false);
+
+  const handleToggle = () => {
+    setToggleAdventures(!toggleAdventures);
+  };
 
   return (
     <div>
-      <ToggleSwitch label=' ' handleToggle={handleToggle}/>
-      {toggleAdventures ? null : <Search attractionSearch={attractionSearch} handleSearch={handleSearch} />}
-      {toggleAdventures ? 'Adventures Placeholder' : <AttactionsContainer attractions={attractions} filteredList={filteredList} />}
+      <ToggleSwitch label=' ' handleToggle={handleToggle} />
+      {!toggleAdventures && <Search attractionSearch={attractionSearch} handleSearch={handleSearch} />}
+      {!toggleAdventures && (
+        <AttractionsContainer
+          filteredList={filteredList}
+          currentPage={currentPage}
+          attractionsPerPage={attractionsPerPage}
+        />
+      )}
+      {toggleAdventures ? 'Adventures Placeholder' : (
+        <div>
+          <button onClick={handlePreviousPage} disabled={currentPage === 1}>Previous</button>
+          <button onClick={handleNextPage} disabled={currentPage === Math.ceil(filteredList.length / attractionsPerPage)}>Next</button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
