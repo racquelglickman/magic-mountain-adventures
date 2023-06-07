2ed622bf0123cc1d43766fb869392ec40c199984
Patrick Roberts <patrob22@gmail.com>
2023-06-07 12:35:27 -0400

    added more details and button for adding to adventures

--- client/src/components/AttractionDetails.js
+++ client/src/components/AttractionDetails.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from "react-router-dom"
 
 function AttractionDetails() {
   const location = useLocation()
   const state = location.state
-  console.log(state.name)
+  
+  const [onItinerary, setOnItinerary] = useState(false)
 
   const placeholderUrl = "https://w7.pngwing.com/pngs/585/718/png-transparent-six-flags-great-adventure-six-flags-great-america-six-flags-magic-mountain-six-flags-over-georgia-six-flags-america-park-text-photography-logo.png"
+
+  function handleClick(e) {
+    console.log(e)
+  }
 
   return (
     <div>
@@ -20,6 +25,10 @@ function AttractionDetails() {
       <p>Average Wait Time: N/A</p>}
       {state.avg_rating ? <p>Average User Rating: {state.avg_rating}</p> : 
       <p>Average User Rating: N/A</p>}
+      
+      <p>THIS WILL EVENTUALLY BE A DESCRIPTION OF HOW AWESOME THIS RIDE IS!!!  IMAGINE A LONG PARAGRAPH ABOUT HOW SUPERMAN OR BATMAN OR WHATEVER NEED YOUR HELP SOLVING A CRIME AND THE ONLY WAY TO DO THAT IS TO BOARD A ROLLER COASTER TO STOP LEX LUTHOR OR SOMETHING.  YEAH!  YOU'RE A NEW MEMBER OF THE JUSTICE LEAGUE AND SAVING THE WORLD ONE THRILL AT A TIME!  OR BY RIDING A CAROUSEL.... WHATEVER.</p>
+      
+      <button onClick={handleClick} >Add to Adventures</button>
     </div>
   )
 }
