5606b00274922fa32f8c1bedef774046da4d6bdf
Patrick Roberts <patrob22@gmail.com>
2023-06-08 13:02:58 -0400

    popups render on markers

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -10,7 +10,6 @@ function Map({ toggle, adventures }) {
   const center = useMemo(() => ({ lat: 34.4243, lng: -118.5973 }), [])
   const { attractions } = useContext(MyContext)
   
-  
   return (
     <div className='mapbox'>
         <MapContainer center={center} zoom={16}>
@@ -19,9 +18,26 @@ function Map({ toggle, adventures }) {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
             {toggle ? 
-            adventures.map((a) => {return <Marker key={a.id} position={[a.attraction.latitude, a.attraction.longitude]} />}) :
-            attractions.map((a) => {return <Marker key={a.id} position={[a.latitude, a.longitude]} />})
-          }
+            adventures.map((a) => {
+              return <Marker key={a.id} position={[a.attraction.latitude, a.attraction.longitude]}>
+                <Popup>
+                  <div>
+                    <h3>{a.attraction.name}</h3>
+                    <p>Thrill Level: {a.attraction.thrill_level}</p>
+                  </div>
+                </Popup>
+              </Marker>
+            }) : attractions.map((a) => {
+              return <Marker key={a.id} position={[a.latitude, a.longitude]}>
+                <Popup>
+                  <div>
+                    <h3>{a.name}</h3>
+                    <p>Thrill Level: {a.thrill_level}</p>
+                  </div>
+                </Popup>
+              </Marker>
+            })}
+
         </MapContainer>
     </div>
   )
