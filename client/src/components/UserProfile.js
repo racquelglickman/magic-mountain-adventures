1f2ae0a8250f650f4f95c26f9215d3cad0aef27c
Jolie Gielchinsky <jolieschae@gmail.com>
2023-06-08 13:17:44 -0400

    adjusted fonts

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -12,12 +12,12 @@ function UserProfile() {
       <div className="main">
         <img className="avatar" src="https://i.imgur.com/3KtXQ0Y.png" alt="avatar" />
         <div className="userInfo">
-        <p className="name">Name: <span className="userInput">{user.first_name} {user.last_name}</span></p>
-        <p className="height">Height: <span className="userInput">{user.height} in.</span></p>
+        <p className="name">Name<span className="userInput">: {user.first_name} {user.last_name}</span></p>
+        <p className="height">Height<span className="userInput">: {user.height} in.</span></p>
       </div>
       </div>
       <div className="historyContainer"> 
-      <p className="adventureTitle">Adventure History:</p>
+      <p className="adventureTitle">Adventure History</p>
         </div>
     </div>
   )
