64f90001ff96e6028e1b95161f3391ad1f3d23d5
Patrick Roberts <patrob22@gmail.com>
2023-06-09 13:43:56 -0400

    bold text in details

--- client/src/components/AttractionDetails.js
+++ client/src/components/AttractionDetails.js
@@ -70,9 +70,9 @@ function AttractionDetails() {
             </p>
       {attraction.height_req > 0 ? <p className="heightReq" >Minimum Height Required: <span className="output">{attraction.height_req} inches</span></p>: 
       <p className="heightReq">Minimum Height Required: <span className="output"> None</span></p>}
-      {attraction.avg_wait ? <p>Average Wait Time: {attraction.avg_wait} minutes</p> : 
+      {attraction.avg_wait ? <p><span className="heightReq">Average Wait Time:</span> {attraction.avg_wait} minutes</p> : 
       <p>Average Wait Time: N/A</p>}
-      {attraction.avg_rating ? <p>Average User Rating: {attraction.avg_rating} / 5.0</p> : 
+      {attraction.avg_rating ? <p><span className="heightReq">Average User Rating:</span> {attraction.avg_rating} / 5.0</p> : 
       <p>Average User Rating: N/A</p>}
       
       <div className="detailsDescription">
