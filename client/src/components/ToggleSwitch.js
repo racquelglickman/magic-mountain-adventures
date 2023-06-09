da26491bfef625329b8de4a3eab154cd85fc5d22
Jolie Gielchinsky <jolieschae@gmail.com>
2023-06-09 10:01:35 -0400

    details page, search bar css

--- client/src/components/ToggleSwitch.js
+++ client/src/components/ToggleSwitch.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./ToggleSwitch.css";
+import './homePage.css'
   
 const ToggleSwitch = ({label, handleToggle}) => {
 
   return (
-    <div className="container">
+    <div className="toggleContainer">
       {label}{" "}
       <div className="toggle-switch">
         <input type="checkbox" className="checkbox" 
